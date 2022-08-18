62b2a4bb50f1c817bb5e27d31338ef43ca763508
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-08-18 21:15:46 +0100

    push

---
+++ src/Content.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="converter">
+      <div className="converter-content">
+        <h1>CONVERTER</h1>
+        <div className="background-convert"></div>
+      </div>
+      <div className="news">
+        <a href="#">News about currency rates<i class="fas fa-arrow-right"></i></a>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='navbar'>
+        <div className="nav-content">
+            <div className="nav-brand">
+                <h1>change<span>Am</span>.</h1>
+            </div>
+            <a href="#">about</a>
+        </div>
+        
+
+    </nav>
+  )
+}
+
+export default Header
