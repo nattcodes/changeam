7449b2d655c59ef79b8b6a52c8853a94ae58cca6
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-08-19 11:37:42 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import React from "react";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer/>
-    </div>
-  );
+    return ( 
+        <div className = "App" >
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
