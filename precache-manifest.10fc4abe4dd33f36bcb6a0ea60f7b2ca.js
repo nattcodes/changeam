1dc455f94fb4a2ff9cf751d2d5d2f7221f100ee9
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-08-19 12:02:13 +0100

    Updates

---
+++ precache-manifest.10fc4abe4dd33f36bcb6a0ea60f7b2ca.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "20a704c0c5dac1b31a1a",
+    "url": "/changeam/static/css/main.33b4e1da.chunk.css"
+  },
+  {
+    "revision": "20a704c0c5dac1b31a1a",
+    "url": "/changeam/static/js/main.20a704c0.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/changeam/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "013178ca9e6e63e40b70",
+    "url": "/changeam/static/js/runtime~main.013178ca.js"
+  },
+  {
+    "revision": "59c3685a73f0f1b7c302dd2d6dabd628",
+    "url": "/changeam/static/media/Courgette-Regular.59c3685a.ttf"
+  },
+  {
+    "revision": "a2d502040b92592e04a3436256fa0e5c",
+    "url": "/changeam/static/media/SansitaSwashed-VariableFont_wght.a2d50204.ttf"
+  },
+  {
+    "revision": "1052d6ca3993ae24a932304560a4c8b4",
+    "url": "/changeam/static/media/Abel-Regular.1052d6ca.ttf"
+  },
+  {
+    "revision": "686d9685b805c407876cd1abd698fd73",
+    "url": "/changeam/static/media/Livvic-Regular.686d9685.ttf"
+  },
+  {
+    "revision": "1191108eb1c36cfecb982299c4e2da13",
+    "url": "/changeam/static/media/AveriaSerifLibre-Regular.1191108e.ttf"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/changeam/static/media/Bangers-Regular.a82040ec.ttf"
+  },
+  {
+    "revision": "bd27b3ec97d12655c7a2803cb6464924",
+    "url": "/changeam/static/media/BeVietnam-Regular.bd27b3ec.ttf"
+  },
+  {
+    "revision": "bafd78da573962d22a817cf22678c8dc",
+    "url": "/changeam/static/media/KaushanScript-Regular.bafd78da.ttf"
+  },
+  {
+    "revision": "7059acee3b9d8ea9744c26ab45135a8b",
+    "url": "/changeam/static/media/SpecialElite-Regular.7059acee.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/changeam/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/changeam/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "561246a3549d3b797b4b611652a79d7c",
+    "url": "/changeam/static/media/Montserrat-Black.561246a3.otf"
+  },
+  {
+    "revision": "433ac418a603294fc5891dde6a73cd24",
+    "url": "/changeam/static/media/Montserrat-ExtraBold.433ac418.otf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/changeam/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "939b732d4342c480df47a24b1f5c306c",
+    "url": "/changeam/static/media/EastSeaDokdo-Regular.939b732d.ttf"
+  },
+  {
+    "revision": "2aedaaeee01141eee27d807b4773abab",
+    "url": "/changeam/index.html"
+  }
+];
